4fbc1b9accb4e55381696770a4296d2cf7c21a2a
tina3008 <kicja_3@ukr.net>
2024-02-29 15:57:43 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+iziToast.error({ 
+    color: 'red',
+    position: "topRight",          
+    message: `❌ Rejected promise`
+})
